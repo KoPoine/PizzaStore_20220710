71b040b1d00e9ffc618d259c1616750714041fd2
김민철 <kmc901206@gmail.com>
2022-07-10 16:11:08 +0900

    2. 좌측 피자가게 Frag 작성  - Fragment xml ListView 태그 작성  - 한칸 xml 작성 완료  - 데이터 클래스 작성  - ListView의 Adapter 작성  - ListView 연결 완료

--- app/src/main/java/com/neppplus/pizzastore_20220710/fragments/PizzaFragment.kt
+++ app/src/main/java/com/neppplus/pizzastore_20220710/fragments/PizzaFragment.kt
@@ -6,8 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.pizzastore_20220710.R
+import com.neppplus.pizzastore_20220710.StoreData
+import com.neppplus.pizzastore_20220710.adapters.StoreListAdapter
+import kotlinx.android.synthetic.main.fragment_pizza.*
 
 class PizzaFragment : Fragment() {
+
+    lateinit var mStoreListAdapter : StoreListAdapter
+    val mList = ArrayList<StoreData>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +21,22 @@ class PizzaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_pizza, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        mList.add(StoreData("피자헛", 4.2, "1588-5588"))
+        mList.add(StoreData("파파존스 ", 4.5, "1577-8080"))
+        mList.add(StoreData("미스터피자 ", 3.8, "1577-0077"))
+        mList.add(StoreData("도미노피자 ", 4.0, "1577-3082"))
+
+        mStoreListAdapter = StoreListAdapter(requireContext(), R.layout.store_list_item, mList)
+        pizzaStoreListView.adapter = mStoreListAdapter
+
+        pizzaStoreListView.setOnItemClickListener { adapterView, view, i, l ->
+
+        }
     }
 
 }
