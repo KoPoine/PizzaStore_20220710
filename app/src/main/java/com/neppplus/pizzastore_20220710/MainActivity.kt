697c4a489bbead0137d68fed429daa78db1a1893
김민철 <kmc901206@gmail.com>
2022-07-10 15:54:51 +0900

    1. ViewPager + TabLayout  - activity xml에 태그 배치  - 프래그먼트 xml 작성  - kt 파일 작성  - ViewPagerAdapter 작성 완료  - ViewPager , TabLayout 연동 완료

--- app/src/main/java/com/neppplus/pizzastore_20220710/MainActivity.kt
+++ app/src/main/java/com/neppplus/pizzastore_20220710/MainActivity.kt
@@ -2,10 +2,20 @@ package com.neppplus.pizzastore_20220710
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.pizzastore_20220710.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mPagerAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mPagerAdapter
+
+        mainTabLayout.setupWithViewPager(mainViewPager)
     }
 }
